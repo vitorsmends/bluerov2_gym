3d6d419934d53f209d6b381bfb8548ee65fb55e7
mendes <vitorsmendscod@gmail.com>
2026-07-16 19:51:46 -0300

    update

--- path_tracking_experiments/run_all.py
+++ path_tracking_experiments/run_all.py
@@ -9,12 +9,10 @@ from pathlib import Path
 
 
 CONTROLLERS = [
-    # ("PID", "run_pid.py"),
-    # ("PPO", "run_ppo.py"),
-    # ("MPC", "run_mpc.py"),
+    ("PID", "run_pid.py"),
+    ("PPO", "run_ppo.py"),
     ("NMPC", "run_nmpc.py"),
-    # ("LQR", "run_lqr.py"),
-    # ("SMC", "run_smc.py"),
+    ("SMC", "run_smc.py"),
 ]
 
 
